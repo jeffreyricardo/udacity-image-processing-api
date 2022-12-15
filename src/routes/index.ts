3415c22bbe836086ef31e67616011b90b89a2f6e
Jeffrey Ricardo <jeffreyricardo@gmail.com>
2022-12-14 22:52:14 -0500

    Updated for better user experience

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,7 +4,9 @@ import images from './api/images';
 const routes = express.Router();
 
 routes.get('/', (req, resp) => {
-  resp.send('Main route...');
+  resp.send(
+    `Please use endpoint at <b>http://${req.hostname}:3000/api/images</b>`
+  );
 });
 
 routes.use('/images', images);
