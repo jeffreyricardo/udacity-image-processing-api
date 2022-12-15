cd5c428b62fb125c784e0cb8ec6c3c1da3bda033
Jeffrey Ricardo <jeffreyricardo@gmail.com>
2022-12-15 10:41:50 -0500

    Removed unnecssary variable

--- src/util/fileutil.ts
+++ src/util/fileutil.ts
@@ -1,6 +1,5 @@
 import { PathLike } from 'fs';
 const fs = require('fs');
-const sharp = require('sharp');
 
 export function checkFileExistsSync(filepath: PathLike): boolean {
   const flag = fs.existsSync(filepath, fs.constants.F_OK);
