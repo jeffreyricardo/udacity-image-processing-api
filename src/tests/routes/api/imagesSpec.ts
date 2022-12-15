8b199cf4c3e135d94a7e6e8e8401f7d6fa3738b6
Jeffrey Ricardo <jeffreyricardo@gmail.com>
2022-12-15 09:29:09 -0500

    Remove any generated pre-existing test files

--- src/tests/routes/api/imagesSpec.ts
+++ src/tests/routes/api/imagesSpec.ts
@@ -9,12 +9,29 @@ const testFileName = 'frenchie_300x200.jpg';
 const destFilePath = path.join(__dirname, thumbDir);
 
 describe('Testing of image functionality for file generation', () => {
+  // Get rid of any pre-existing test files first
+  beforeAll(() => {
+    console.log(
+      'Preparing test environment.  Removing pre-existing generated thumbs'
+    );
+    try {
+      fs.unlinkSync(destFilePath + testFileName);
+    } catch (err) {
+      console.log('Error removing test files - ' + err);
+    }
+  });
+
   it('Testing of image with proper parameters ', async () => {
+    // Hit the API with good params and generate a thumbnail
     const response = await supertest(app).get(
       '/api/images?filename=frenchie&width=300&height=200'
     );
-    const files: string[] = fs.readdirSync(destFilePath);
+
+    // Expect a file successful response
     expect(response.status).toBe(200);
+
+    // Expect the file to be present in the thumb directory
+    const files: string[] = fs.readdirSync(destFilePath);
     expect(files.includes(testFileName)).toBeTrue();
   });
 });
