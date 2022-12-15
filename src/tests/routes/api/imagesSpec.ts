e9f788f31dce6fe457d114bf47fc27124a9c6bd6
Jeffrey Ricardo <jeffreyricardo@gmail.com>
2022-12-15 10:57:52 -0500

    Cleaned up test scripts

--- src/tests/routes/api/imagesSpec.ts
+++ src/tests/routes/api/imagesSpec.ts
@@ -5,26 +5,15 @@ const fs = require('fs');
 const path = require('path');
 
 const thumbDir = '../../../../assets/thumb/';
-const testFileName = 'frenchie_300x200.jpg';
+const testFileName = 'frenchie_400x400.jpg';
 const destFilePath = path.join(__dirname, thumbDir);
 
 describe('Testing of image functionality for file generation', (): void => {
-  // Get rid of any pre-existing test files first
-  beforeAll(() => {
-    console.log(
-      'Preparing test environment.  Removing pre-existing generated thumbs'
-    );
-    try {
-      fs.unlinkSync(destFilePath + testFileName);
-    } catch (err) {
-      console.log('Error removing test files - ' + err);
-    }
-  });
 
-  it('Testing of image with proper parameters ', async (): Promise<void> => {
+  it('Receive 200 response code AND that file exists after image processing', async (): Promise<void> => {
     // Hit the API with good params and generate a thumbnail
     const response = await supertest(app).get(
-      '/api/images?filename=frenchie&width=300&height=200'
+      '/api/images?filename=frenchie&width=400&height=400'
     );
 
     // Expect a file successful response
@@ -33,5 +22,17 @@ describe('Testing of image functionality for file generation', (): void => {
     // Expect the file to be present in the thumb directory
     const files: string[] = fs.readdirSync(destFilePath);
     expect(files.includes(testFileName)).toBeTrue();
+  });
+
+  //Get rid of any files generated during test script
+  afterAll(() => {
+    console.log(
+      'Cleaning up test environment. Removing pre-existing generated thumbs'
+    );
+    try {
+      fs.unlinkSync(destFilePath + testFileName);
+    } catch (err) {
+      console.log('Error removing test files - ' + err);
+    }
   });
 });
